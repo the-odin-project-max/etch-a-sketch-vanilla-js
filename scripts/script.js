24bb8899f2b223cdaf2010c81b7cdafd80757520
Max <max.teiger@gmail.com>
2023-08-25 16:20:59 +0200

    feat(controls): add functionality to controls options

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,4 @@
-function createGrid(numOfSquares) {
-	let canva = document.querySelector('.canva');
-	canva.textContent = '';
-
-	for (let i = 0; i < numOfSquares; i++) {
-		for (let j = 0; j < numOfSquares; j++) {
-			const square = document.createElement('div');
-			square.classList.add('square');
-			square.style.width = `${100 / numOfSquares}%`;
-			square.style.height = `${100 / numOfSquares}%`;
-			canva.appendChild(square);
-			initHover();
-		}
-	}
-}
-
-function clearGrid() {
-	document.querySelectorAll(".square").forEach(square => {
-		square.style.backgroundColor = 'white';
-	});
-}
-
-createGrid(2);
+const SQUARE_STYLE_BORDER = '1px solid lightgray';
 
 function changeColor(e) {
 	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -33,4 +11,59 @@ function initHover() {
 	});
 }
 
+// Change grid size based on slider
+let rangeInput = document.getElementById("size");
+
+rangeInput.addEventListener("change", function() {
+    document.querySelector(".selected-size").textContent = rangeInput.value;
+	createGrid(rangeInput.value);
+}, false);
+// ******* //
+
+// Reset grid
 document.querySelector('.reset').addEventListener('click', clearGrid);
+function clearGrid() {
+	document.querySelectorAll(".square").forEach(square => {
+		square.style.backgroundColor = 'white';
+	});
+}
+// ******* //
+
+// Add grid square borders
+let gridToggle = document.querySelector("input[name=grid-toggle]");
+
+gridToggle.addEventListener('change', function() {
+  if (this.checked) {
+	document.querySelectorAll(".square").forEach(square => {
+		square.style.border = SQUARE_STYLE_BORDER;
+	});
+  } else {
+	document.querySelectorAll(".square").forEach(square => {
+		square.style.border = 'none';
+	});  }
+});
+// ******* //
+
+// Init grid
+function createGrid(numOfSquares) {
+	let canva = document.querySelector('.canva');
+	let gridToggle = document.querySelector("input[name=grid-toggle]");
+
+	canva.textContent = '';
+
+	for (let i = 0; i < numOfSquares; i++) {
+		for (let j = 0; j < numOfSquares; j++) {
+			const square = document.createElement('div');
+			square.classList.add('square');
+			square.style.width = `${100 / numOfSquares}%`;
+			square.style.height = `${100 / numOfSquares}%`;
+			if (gridToggle.checked) {
+				square.style.border = SQUARE_STYLE_BORDER;
+			}
+			canva.appendChild(square);
+			initHover();
+		}
+	}
+}
+createGrid(rangeInput.value);
+// ******* //
